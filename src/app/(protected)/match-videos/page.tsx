a1902bf27a944d3e53c79c2715d08ed2b11d1990
GuillermoSH <siciliahernandezguillermo@gmail.com>
2025-09-24 16:22:21 +0100

    :construction: Trabajo en progreso

--- src/app/(protected)/match-videos/page.tsx
+++ src/app/(protected)/match-videos/page.tsx
@@ -5,7 +5,7 @@ export default async function PartidosPage() {
   const videos = await getVideosByCategory("match");
 
   return (
-    <main className="min-h-screen p-6">
+    <main className="min-h-screen bg-gray-100 dark:bg-gray-600 p-6">
       <h1 className="text-2xl font-bold mb-6">Videos de Partidos</h1>
       {videos.length > 0 ? (
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -11,24 +11,26 @@ export default async function VideoCard({ url, category }: Readonly<VideoCardPro
     const title = await getYoutubeTitle(videoId);
 
     return (
-        <div className="bg-white rounded-lg shadow p-4 flex flex-col">
-            <Image
-                src={getThumbnailUrl(url, "hq")}
-                alt={title || "Video"}
-                width={320}
-                height={180}
-                className="w-full h-auto rounded"
-            />
-            <h2 className="mt-2 text-lg font-bold">{title || "Cargando..."}</h2>
-            <p className="text-sm text-gray-500">{category}</p>
-            <a
-                href={url}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="mt-3 inline-block px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition"
-            >
-                Ver video
-            </a>
+        <div className="border border-white/30 overflow-hidden bg-white/20 backdrop-blur-sm shadow-lg rounded-lg p-4 flex flex-col md:flex-row gap-2">
+            <div className="w-full md:w-1/3 relative aspect-video">
+                <Image
+                    src={getThumbnailUrl(url, "max")}
+                    alt={title || "Video"}
+                    fill
+                    className="object-cover rounded-md"
+                />
+            </div>
+            <div className="flex flex-col justify-between gap-2">
+                <h2 className="text-md font-bold">{title || "Cargando..."}</h2>
+                <a
+                    href={url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="inline-block px-4 py-2 text-md bg-gray-950 text-white rounded-lg hover:bg-gray-800 transition"
+                >
+                    Ver video
+                </a>
+            </div>
         </div>
     );
 }
